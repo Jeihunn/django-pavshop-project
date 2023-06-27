22c8f001ab44c9066ca44d22d6fd17a7da9ad2f1
Jeyhun Piriyev <jeihunpiriyev@gmail.com>
2023-06-27 15:19:47 +0400

    app folder updated

--- pavshop/config/urls.py
+++ pavshop/config/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include('core.urls', namespace="core")),
+    path("", include('account.urls', namespace="account")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
