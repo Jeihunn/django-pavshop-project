67a43295ad21fc32424008be6dece7dc353c7029
Jeyhun Piriyev <jeihunpiriyev@gmail.com>
2023-06-27 16:36:13 +0400

    templates, static completed

--- pavshop/core/views.py
+++ pavshop/core/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, "core/index.html")
+
+
+def contact_view(request):
+    return render(request, "core/contact.html")
+
+
+def checkout_view(request):
+    return render(request, "core/checkout.html")
+
+
+def about_us_view(request):
+    return render(request, "core/about-us.html")
