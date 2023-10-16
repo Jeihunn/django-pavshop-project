0d2f4b8416bf0b9a4253f17adb2b064110ef29ee
ncniyazov <75684807+ncniyazov@users.noreply.github.com>
2023-10-16 14:47:36 +0400

    Update urls.py

--- pavshop/core/urls.py
+++ pavshop/core/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index_view, name="index_view"),
     path("contact/", views.ContactView.as_view(), name="contact_view"),
     path("about-us/", views.about_us_view, name="about_us_view"),
-    path("export/", views.export_reviews, name="export_reviews"),
+    path("backgound-task-testing/",
+         views.backgound_task_view, name="backgound_task_view"),
 ]
