45663bd19b6b2fa3cbe0a8828f6ee30f6ed5eef8
ncniyazov <75684807+ncniyazov@users.noreply.github.com>
2023-10-18 23:36:28 +0400

    Update project_secrets.py verified profile

--- pavshop/config/project_secrets.py
+++ pavshop/config/project_secrets.py
@@ -7,5 +7,6 @@ google_key = '729969587540-6t0iamcnlinrhaphs36rt3fjmf4vrob8.apps.googleuserconte
 google_secret = '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'
 
 # Facebook OAuth2 key and secret configuration
-facebook_key = 277567418235931                        # App ID
-facebook_secret = 'e9663b5f6c1dc60b1f22002705249858'    # App Secret
+facebook_key = 3633521370260723                         # App ID  verified
+facebook_secret = '8e87e501c5e3952b917e86da60a641f2'    # App Secret
+
